34e6fdc5de80a1779683bd7e131829e68638fbc8
Meng Wang <mengwangk@gmail.com>
2021-06-17 12:19:11 +0800

    Update app.py

--- app-reloading/python/app.py
+++ app-reloading/python/app.py
@@ -1,7 +1,14 @@
 from lib import func1, func2
 
+x = 3
+y = 9
+str = "hello alpha2phi"
+
 print("Calling main app...")
 
 print("Callings library function...")
 func1()
 func2()
+
+z = x + y
+print(z)
